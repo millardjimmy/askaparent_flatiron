f179f2ccfbe2f0c949b4a567f8c79a1648f97a93
millardjimmy <millardjimmy@gmail.com>
2020-07-30 12:53:00 -0600

    authentication methods added to applicationCONT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        if logged_in?
+            User.find(session[:user_id])
+        else
+            false
+        end
+    end
+
+    def logged_in?
+        session[:user_id]
+    end
+
 end

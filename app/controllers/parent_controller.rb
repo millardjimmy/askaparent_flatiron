d489da4b39a8f066faef70b720d24ab39fd81c4a
millardjimmy <millardjimmy@gmail.com>
2020-07-30 15:04:01 -0600

    expansion using params

--- app/controllers/parent_controller.rb
+++ app/controllers/parent_controller.rb
@@ -1,13 +1,17 @@
-class ParentController < ApplicationController
-
+class UsersController < ApplicationController
     def create
-        @user = User.create
-        redirect_to questions_path
+      @user = User.create(user_params)
+      return render 'new' unless @user.save
+  
+      session[:user_id] = @user.id
+      redirect_to questions_path
     end
-
+  
     def new
-        @user = User.new
+      @user = User.new
     end
-
-
-end
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+  end

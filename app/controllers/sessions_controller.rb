0baf33066be2930f767e34071eb43039086eb608
millardjimmy <millardjimmy@gmail.com>
2020-07-30 13:00:02 -0600

    session activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        if  session[:user_id] = user.id 
+            redirect_to questions_path
+        else
+            redirect_to '/login'
+        end
+    end
+
+    def destroy
+        session.destroy
+        redirect_to questions_path
+    end
+
 end

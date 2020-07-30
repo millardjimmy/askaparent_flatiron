20da8aa4e71c2766251c5307d3bf749c91b3bf7e
millardjimmy <millardjimmy@gmail.com>
2020-07-30 12:40:26 -0600

    create and new method added to parent controller

--- app/controllers/parent_controller.rb
+++ app/controllers/parent_controller.rb
@@ -1,2 +1,13 @@
 class ParentController < ApplicationController
+
+    def create
+        @user = User.create
+        redirect_to questions_path
+    end
+
+    def new
+        @user = User.new
+    end
+
+
 end
